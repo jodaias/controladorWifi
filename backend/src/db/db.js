83bcdc65a517f0f7c5d6dec4c2d9531dfc0abb92
Jodaías <jodaias2013@gmail.com>
2020-11-29 01:24:26 -0300

    detalhes excluidos

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -18,13 +18,13 @@ async function selectUsers() {
 
 async function selectUser(email) {
   const conn = await connect();
-  const sql = 'SELECT id, nome, email, whatsapp, token FROM users  where email=?;';
+  const sql = 'SELECT id, nome, email, whatsapp FROM users  where email=?;';
   const [row] = await conn.query(sql, [email]);
   return row;
 }
 
 async function insertUser(user) {
-  console.log('aqui passou 32');
+
   const conn = await connect();
   const sql = 'INSERT INTO users(nome,email,whatsapp) VALUES (?,?,?);';
   const values = [user.nome, user.email, user.whatsapp];
