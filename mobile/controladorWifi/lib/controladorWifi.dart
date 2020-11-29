99a28cfbafa02c940611f531a3a30ed0b4b60ea0
eRoberio <roberioalmeida.12@hotmail.com>
2020-11-29 00:23:18 -0300

    Commit com Mobile

---
+++ mobile/controladorWifi/lib/controladorWifi.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ControllerWifi extends StatefulWidget {
+  @override
+  _ControllerWifiState createState() => _ControllerWifiState();
+}
+
+class _ControllerWifiState extends State<ControllerWifi> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
